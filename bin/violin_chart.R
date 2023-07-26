90bb228237a2ef73fcd470a9e7eb081d0d58252b
replikation <christian.jena@gmail.com>
2023-07-26 16:38:05 +0200

    fixed y axis

--- bin/violin_chart.R
+++ bin/violin_chart.R
@@ -9,7 +9,7 @@ df1 <- read_tsv("input.tsv", col_names=names)
 plot <- ggplot(df1, aes(x=deg, y=ratio, fill=deg)) +
   scale_fill_brewer(palette="Blues") +
   geom_violin(alpha=0.4, position = position_dodge(width = .75),show.legend = FALSE) +
-  coord_cartesian(ylim=c(0,50)) +
+  coord_cartesian(ylim=c(0,1)) +
   #stat_summary(fun.y=mean, geom="point", shape=23, size=2) +
   #stat_summary(fun.y=median, geom="point", size=2, color="red") +
   #geom_dotplot(binaxis='y', stackdir='center', dotsize=0.5, color='black') +
