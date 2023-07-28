f9de036f656964a82b741f56e8cf39ec8caa546c
replikation <christian.jena@gmail.com>
2023-07-28 13:17:53 +0200

    adjustments to plot

--- bin/violin_chart.R
+++ bin/violin_chart.R
@@ -3,10 +3,10 @@
 library(ggplot2)
 require(readr)
 
-names <- c('deg', 'base', 'ratio')
+names <- c('deg', 'Bases', 'ratio')
 df1 <- read_tsv("input.tsv", col_names=names)
 
-plot <- ggplot(df1, aes(x=base, y=ratio, fill=base)) +
+plot <- ggplot(df1, aes(x=Bases, y=ratio, fill=Bases)) +
   scale_fill_brewer(palette="Set2") +
   geom_violin(alpha=0.2, position = position_dodge(width = .75)) +
   coord_cartesian(ylim=c(0.2,0.8)) +
@@ -15,8 +15,8 @@ plot <- ggplot(df1, aes(x=base, y=ratio, fill=base)) +
   #geom_dotplot(binaxis='y', stackdir='center', dotsize=0.5, color='black') +
   geom_point( shape = 21,size=2, position = position_jitterdodge(), color="black",alpha=0.2, show.legend = FALSE) +
   facet_grid(cols = vars(deg),scales = "free", space = "free") +
-  ylab(  c("Base ratio")  ) +
-  xlab(  c("masked Base")  ) +
+  ylab(  c("Base fraction for each affected position")  ) +
+  xlab(  c("Observed types of base combination - labeled by their respective IUPAC code")  ) +
   theme_classic() +
   theme(legend.position="bottom") 
 
