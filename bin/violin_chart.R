0758e1622b5c941d57f2a90ac7d45d869cbe2235
replikation <christian.jena@gmail.com>
2024-02-13 10:32:13 +0100

    improved violin chart

--- bin/violin_chart.R
+++ bin/violin_chart.R
@@ -8,11 +8,11 @@ df1 <- read_tsv("input.tsv", col_names=names)
 
 plot <- ggplot(df1, aes(x=orientation, y=ratio)) +
   scale_fill_brewer(palette="Accent") +
-  geom_violin(alpha=0.2, position = position_dodge(width = .75)) +
-  coord_cartesian(ylim=c(0,0.8)) +
-  scale_y_continuous(breaks = seq(0,0.8, by=0.1)) +
   geom_point( aes(x=orientation, y=ratio, colour=Bases), shape = 16,size=2, position = position_jitterdodge(jitter.width = 0.8, jitter.height = 0, dodge.width = 0), alpha=0.2, show.legend = TRUE) +
   scale_color_manual(values = c("#24305E", "#00CCCC", "#f43c3c", "#FBAC23")) +   # alternative #5C5C5C
+  coord_cartesian(ylim=c(0,0.8)) +
+  scale_y_continuous(breaks = seq(0,0.8, by=0.1)) +
+  geom_violin(alpha=0.2, position = position_dodge(width = .75)) +
   facet_grid(cols = vars(deg), scales = "free", space = "free") +
   ylab(  c("Base fractions of both strands for each ambiguous position")  ) +
   xlab(  c("Strand orientation of each base - determined by aligning each read to their reference")  ) +
